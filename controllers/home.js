eef5f0065356d5a254ff88725ef8be8767f75dea
Sr0121 <532835534@qq.com>
2018-06-16 10:51:07 +0800

    优化主界面

--- controllers/home.js
+++ controllers/home.js
@@ -31,6 +31,26 @@ var home_info = async (ctx, next) => {
         map['level_6'] = row[0]['level_6'];
         map['toefl'] = row[0]['toefl'];
         map['IELTS'] = row[0]['IELTS'];
+        map['level_4_total'] = row[0]['level_4_total'];
+        map['level_4_learned'] = row[0]['level_4_learned'];
+        map['level_4_review_target'] = row[0]['level_4_review_target'];
+        map['level_4_review_learned'] = row[0]['level_4_review_learned'];
+        map['level_4_left'] = row[0]['level_4_left'];
+        map['level_6_total'] = row[0]['level_6_total'];
+        map['level_6_learned'] = row[0]['level_6_learned'];
+        map['level_6_review_target'] = row[0]['level_6_review_target'];
+        map['level_6_review_learned'] = row[0]['level_6_review_learned'];
+        map['level_6_left'] = row[0]['level_6_left'];
+        map['toefl_total'] = row[0]['toefl_total'];
+        map['toefl_learned'] = row[0]['toefl_learned'];
+        map['toefl_review_target'] = row[0]['toefl_review_target'];
+        map['toefl_review_learned'] = row[0]['toefl_review_learned'];
+        map['toefl_left'] = row[0]['toefl_left'];
+        map['IELTS_total'] = row[0]['IELTS_total'];
+        map['IELTS_learned'] = row[0]['IELTS_learned'];
+        map['IELTS_review_target'] = row[0]['IELTS_review_target'];
+        map['IELTS_review_learned'] = row[0]['IELTS_review_learned'];
+        map['IELTS_left'] = row[0]['IELTS_left'];
     }
     ctx.response.status = 200;
     ctx.response.type = 'application/json';
--- views/js/home.js
+++ views/js/home.js
@@ -50,6 +50,7 @@ $(document).ready(function(){
             $.ajax({url:'/home/change', type:'post' , dataType:'json' , data:{"type":"level_4", "num":$('#home_learn').val()} , async:false, success:function(data){
             }});
             $('#home_learn').val("");
+            $(location).attr('href', './home');
         }
         else{
             $('#home_learn').val("");
@@ -63,6 +64,7 @@ $(document).ready(function(){
             $.ajax({url:'/home/change', type:'post' , dataType:'json' , data:{"type":"level_6", "num":$('#home_learn').val()} , async:false, success:function(data){
                 }});
             $('#home_learn').val("");
+            $(location).attr('href', './home');
         }
         else{
             $('#home_learn').val("");
@@ -76,6 +78,7 @@ $(document).ready(function(){
             $.ajax({url:'/home/change', type:'post' , dataType:'json' , data:{"type":"toefl", "num":$('#home_learn').val()} , async:false, success:function(data){
                 }});
             $('#home_learn').val("");
+            $(location).attr('href', './home');
         }
         else{
             $('#home_learn').val("");
@@ -89,6 +92,7 @@ $(document).ready(function(){
             $.ajax({url:'/home/change', type:'post' , dataType:'json' , data:{"type":"IELTS", "num":$('#home_learn').val()} , async:false, success:function(data){
                 }});
             $('#home_learn').val("");
+            $(location).attr('href', './home');
         }
         else{
             $('#home_learn').val("");
@@ -99,52 +103,116 @@ $(document).ready(function(){
 
 
 $(document).ready(function(){
+    $.ajax({url:'/home/info' , type:'get', success:function(data){
+        if (data['state'] == 'SUCCESS') {
+            $("#home_level_4_total").text(data['level_4_total']);
+            $("#home_level_6_total").text(data['level_6_total']);
+            $("#home_toefl_total").text(data['toefl_total']);
+            $("#home_IELTS_total").text(data['IELTS_total']);
+            if(data['level_4']+data['level_4_review_target'] === 0){
+                $("#home_level_4_info").text("无计划");
+                $("#home_btn_1").attr("disabled", "disabled");
+            }
+            else{
+                $("#home_level_4_info").html("今日学习目标"+(data['level_4'])+"词");
+                if(data['level_4'] === data['level_4_learned'] && data['level_4_review_target'] === data['level_4_review_learned']){
+                    $("#home_btn_1").attr("disabled", "disabled");
+                    $("#home_btn_1_text").text("完成");
+                }
+            }
+            if(data['level_6']+data['level_6_review_target'] === 0){
+                $("#home_level_6_info").text("无计划");
+                $("#home_btn_2").attr("disabled", "disabled");
+            }
+            else{
+                $("#home_level_6_info").html("今日学习目标"+(data['level_6'])+"词");
+                if(data['level_6'] === data['level_6_learned'] && data['level_6_review_target'] === data['level_6_review_learned']){
+                    $("#home_btn_2").attr("disabled", "disabled");
+                    $("#home_btn_2_text").text("完成");
+                }
+            }
+            if(data['toefl']+data['toefl_review_target'] === 0){
+                $("#home_toefl_info").text("无计划");
+                $("#home_btn_3").attr("disabled", "disabled");
+            }
+            else{
+                $("#home_toefl_info").html("今日学习目标"+(data['toefl'])+"词");
+                if(data['toefl'] === data['toefl_learned'] && data['toefl_review_target'] === data['toefl_review_learned']){
+                    $("#home_btn_3").attr("disabled", "disabled");
+                    $("#home_btn_3_text").text("完成");
+                }
+            }
+            if(data['IELTS']+data['IELTS_review_target'] === 0){
+                $("#home_IELTS_info").text("无计划");
+                $("#home_btn_4").attr("disabled", "disabled");
+            }
+            else{
+                $("#home_IELTS_info").html("今日学习目标"+(data['IELTS'])+"词");
+                if(data['IELTS'] === data['IELTS_learned'] && data['IELTS_review_target'] === data['IELTS_review_learned']){
+                    $("#home_btn_4").attr("disabled", "disabled");
+                    $("#home_btn_4_text").text("完成");
+                }
+            }
 
-    var progObj = radialIndicator('#indicatorContainer1' , {
-        radius: 80,
-        barColor: '#87CEEB',
-        barWidth: 10,
-        initValue: 0,
-        roundCorner : true,
-        percentage: true,
-        displayNumber: false
-    });
-        
-    progObj.animate(30);
+            var progObj = radialIndicator('#indicatorContainer1' , {
+                radius: 80,
+                barColor: '#87CEEB',
+                barWidth: 10,
+                initValue: 0,
+                roundCorner : true,
+                percentage: true,
+                displayNumber: false
+            });
 
-    progObj = radialIndicator('#indicatorContainer2' , {
-        radius: 80,
-        barColor: '#87CEEB',
-        barWidth: 10,
-        initValue: 0,
-        roundCorner : true,
-        percentage: true,
-        displayNumber: false
-    });
-        
-    progObj.animate(30);
+            var rate= (data['level_4_total']-data['level_4_left'])/data['level_4_total']*100;
+            progObj.animate(rate);
+            $("#home_rate1").text(rate.toFixed(2)+"%");
 
-    progObj = radialIndicator('#indicatorContainer3' , {
-        radius: 80,
-        barColor: '#87CEEB',
-        barWidth: 10,
-        initValue: 0,
-        roundCorner : true,
-        percentage: true,
-        displayNumber: false
-    });
-        
-    progObj.animate(30);
+            progObj = radialIndicator('#indicatorContainer2' , {
+                radius: 80,
+                barColor: '#87CEEB',
+                barWidth: 10,
+                initValue: 0,
+                roundCorner : true,
+                percentage: true,
+                displayNumber: false
+            });
 
-    progObj = radialIndicator('#indicatorContainer4' , {
-        radius: 80,
-        barColor: '#87CEEB',
-        barWidth: 10,
-        initValue: 0,
-        roundCorner : true,
-        percentage: true,
-        displayNumber: false
-    });
-        
-    progObj.animate(30);
+            rate= (data['level_6_total']-data['level_6_left'])/data['level_6_total']*100;
+            progObj.animate(rate);
+            $("#home_rate2").text(rate.toFixed(2)+"%");
+
+            progObj = radialIndicator('#indicatorContainer3' , {
+                radius: 80,
+                barColor: '#87CEEB',
+                barWidth: 10,
+                initValue: 0,
+                roundCorner : true,
+                percentage: true,
+                displayNumber: false
+            });
+
+            rate= (data['toefl_total']-data['toefl_left'])/data['toefl_total']*100;
+            progObj.animate(rate);
+            $("#home_rate3").text(rate.toFixed(2)+"%");
+
+            progObj = radialIndicator('#indicatorContainer4' , {
+                radius: 80,
+                barColor: '#87CEEB',
+                barWidth: 10,
+                initValue: 0,
+                roundCorner : true,
+                percentage: true,
+                displayNumber: false
+            });
+
+            rate= (data['IELTS_total']-data['IELTS_left'])/data['IELTS_total']*100;
+            progObj.animate(rate);
+            $("#home_rate4").text(rate.toFixed(2)+"%");
+        }
+        else{
+            $(location).attr('href', './sign_in');
+        }
+    }});
+
 });
